5ddabe8500bcdacf1374449e758eb1a5706b1c73
shishir686 <shishiachar16@gmail.com>
2026-07-14 22:59:47 +0530

    Mesh updated

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-import numpy, scipy, random
+import numpy as np
 
-# Rectangle geometry: defined by 4 corner nodes
+# Rectangle geometry bounds
 x_min, x_max = 0.0, 10.0
 y_min, y_max = 10.0, 20.0
 
-nodes = [
-    (x_min, y_min),  # bottom-left
-    (x_max, y_min),  # bottom-right
-    (x_max, y_max),  # top-right
-    (x_min, y_max),  # top-left
-]
+# Resolution
+nx, ny = 10, 20   # nodes along x and y
 
-cells = [0, 1, 2, 3]  # single cell/loop referencing the 4 corner nodes, in order
+# Node coordinates stored as 2D arrays, indexed [i][j]
+X = np.zeros((nx, ny))
+Y = np.zeros((nx, ny))
 
-print(nodes, cells)
+for i in range(nx):
+    for j in range(ny):
+        X[i, j] = x_min + (x_max - x_min) * i / (nx - 1)
+        Y[i, j] = y_min + (y_max - y_min) * j / (ny - 1)
+
+# Cell connectivity: each cell defined by 4 node index-pairs (i,j)
+# cell(i,j) uses nodes (i,j), (i+1,j), (i+1,j+1), (i,j+1)
+cells = []
+for i in range(nx - 1):
+    for j in range(ny - 1):
+        cell = [(i, j), (i + 1, j), (i + 1, j + 1), (i, j + 1)]
+        cells.append(cell)
+
+print("Grid shape (X):", X.shape)
+print("Grid shape (Y):", Y.shape)
+print("Number of cells:", len(cells))
+print("Sample node (0,0):", (X[0, 0], Y[0, 0]))
+print("Sample cell (0,0):", cells[0])
