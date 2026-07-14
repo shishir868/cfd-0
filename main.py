73b7d5cd9cb667474750baf736fea62a9e095020
shishir686 <shishiachar16@gmail.com>
2026-07-14 22:38:24 +0530

    New code for rectangle

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-#CFD-0 project, Laminar flow
+import numpy, scipy, random
+
+# Rectangle geometry: defined by 4 corner nodes
+x_min, x_max = 0.0, 10.0
+y_min, y_max = 10.0, 20.0
+
+nodes = [
+    (x_min, y_min),  # bottom-left
+    (x_max, y_min),  # bottom-right
+    (x_max, y_max),  # top-right
+    (x_min, y_max),  # top-left
+]
+
+cells = [0, 1, 2, 3]  # single cell/loop referencing the 4 corner nodes, in order
+
+print(nodes, cells)
